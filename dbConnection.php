5b180de05a5555886664175878c4352c9cf54d79
kaysmart001 <luwoyekayode99@gmail.com>
2023-01-29 12:52:36 +0100

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -3,16 +3,16 @@
 date_default_timezone_set("Africa/Lagos");
 ini_set('display_errors', FALSE);
 ini_set('log_errors', TRUE);
-$conn=mysqli_connect('localhost','root','','edu') or die(mysqli_error($conn)); 
+$conn=mysqli_connect('sql311.epizy.com','epiz_33459705','hvZgW4p2guu7','epiz_33459705_cbt_db') or die(mysqli_error($conn)); 
 
- $dsn = "mysql:host=localhost;dbname=edu";
+ $dsn = "mysql:host=sql311.epizy.com;dbname=epiz_33459705_cbt_db";
     try {
-        $pdo = new PDO($dsn, 'root', '');
+        $pdo = new PDO($dsn, 'epiz_33459705', 'hvZgW4p2guu7');
     }
     catch(PDOException $e) {
         echo $e->getMessage();
     }
 
-$mysqli = new mysqli('localhost', 'root', '', 'edu');
+$mysqli = new mysqli('sql311.epizy.com', 'epiz_33459705', 'hvZgW4p2guu7', 'epiz_33459705_cbt_db');
 
 ?>
